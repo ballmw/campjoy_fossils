b5db521758d5a923c034c6e84a9e58feb2499275
Andy Douglas <andrew.douglas11@gmail.com>
2011-10-22 18:57:02 -0400

    Specimen page - looking good

--- www/js/views/Specimen.js
+++ www/js/views/Specimen.js
@@ -22,8 +22,9 @@ App.views.Specimen = Ext.extend(Ext.Panel, {
 			width: 120,
 			handler: function(btn){
 				App.viewport.specimenView.specimenDetailPanel.setWidth(App.viewport.modalWidth);
-				App.viewport.specimenView.specimenDetailPanel.setHeight(App.viewport.modalHeight);					
+				App.viewport.specimenView.specimenDetailPanel.setHeight(App.viewport.modalHeight);
 				App.viewport.specimenView.specimenDetailPanel.show();
+				App.viewport.specimenView.specimenDetailPanel.scroller.scrollTo({x:0,y:0});					
 				App.viewport.specimenView.doComponentLayout();
 			}
 		},{
@@ -39,7 +40,7 @@ App.views.Specimen = Ext.extend(Ext.Panel, {
 	}],
 	initComponent : function() {
 		this.specimenPanel = new Ext.Panel({
-			tpl: Ext.XTemplate.from('specimen')
+			//scroll: true
 		});
 		//modal window
 		this.specimenDetailPanel = new Ext.Panel({
@@ -58,8 +59,72 @@ App.views.Specimen = Ext.extend(Ext.Panel, {
 	},
 	listeners : {
 		show : function() {
-			this.specimenPanel.update(this.specimen);
 			this.specimenDetailPanel.update(this.specimen);
+			
+			this.scroller.scrollTo({x:0, y:0});
+			
+			// Main specimen name and image(s)
+			this.specimenPanel.removeAll();
+			
+			this.specimenPanel.add({
+				html: this.specimen.name
+			});
+			
+			var toggleImagePanel = new Ext.Panel({
+				width: 200,
+				html: '<img src="images/' + this.specimen.photo_image[0] + '" height="120" width="200px" />'
+			});
+			
+			if (typeof(this.specimen.photo_image) == 'string' || this.specimen.photo_image.length === 1) {
+				this.specimenPanel.add({
+					html: '<img src="images/' + ((this.specimen.photo_image.length === 1) ? this.specimen.photo_image[0] : this.specimen.photo_image) + '" width="200px" />'
+				});
+			}
+			else {
+				var me = this;
+				var imageCount = this.specimen.photo_image.length;
+				var currentImageIndex = 0;
+				
+				var toggleImage = function(direction){
+					currentImageIndex += direction;
+					if (currentImageIndex < 0) {
+						currentImageIndex = imageCount - 1;
+					}
+					currentImageIndex %= imageCount;
+					toggleImagePanel.update('<img src="images/' + me.specimen.photo_image[currentImageIndex] + '" height="120" width="200px" />');
+					me.specimenPanel.doComponentLayout();
+				};
+				
+				this.specimenPanel.add(new Ext.Panel({
+					items: [new Ext.Panel({
+						layout: 'hbox',
+						width: '100%',
+						items: [new Ext.Button({
+							text: '&lt;',
+							height: 30,
+							padding:4,
+							flex: 1,
+							handler: function(){
+								toggleImage(-1);
+							}
+						}), toggleImagePanel, new Ext.Button({
+							text: '&gt;',
+							height: 30,
+							padding:4,
+							flex: 1,
+							handler: function(){
+								toggleImage(1);
+							}
+						})]
+					})]
+				}));
+			}
+			
+			this.specimenPanel.add({
+				html: '<br /><div style="padding-right:10px;">' + (this.specimen.short_description || '') + '</div>'
+			});
+			
+			this.specimenPanel.doLayout();
 		}
 	}
 });
