87e8874a1809c7f2899ebea76a0c7fb1ad7f28d2
unknown <aswerlick@CSGCINLT504.cardinalsolutions.com>
2011-10-22 03:30:01 -0400

    adding about page

--- www/js/views/PhoneApp.js
+++ www/js/views/PhoneApp.js
@@ -56,6 +56,7 @@ App.PhoneApp = Ext.extend(Ext.Panel, {
 		this.homeView = new App.views.Home();
 		this.keyView = new App.views.DichotemousKey();
 		this.catalogView = new App.views.Catalog();
+		this.homeView = new App.views.About();
 		this.specimenView = new App.views.Specimen();
 
 		this.items = [this.homeView, this.keyView, this.catalogView, this.specimenView];
@@ -81,6 +82,11 @@ App.PhoneApp = Ext.extend(Ext.Panel, {
 				iconCls : 'catalog',
 				handler : function() {
 					App.viewport.setActiveItem(App.viewport.catalogView)
+				}				
+			}, { text: 'About',
+				iconCls : 'about',
+				handler : function() {
+					App.viewport.setActiveItem(App.viewport.aboutView)
 				}
 			}],
 			layout : {
